4c9e183536cd174ab06329f7e593dc91f763e2e1
Jimmy Castilho <jimmycastilho555@gmail.com>
2025-05-09 09:58:59 -0300

    novas alterações

--- app-clinica-csv/app/Http/Controllers/PacienteController.php
+++ app-clinica-csv/app/Http/Controllers/PacienteController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Paciente;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Barryvdh\DomPDF\Facade\Pdf;
 use App\Exports\PacientesExport;
 
 class PacienteController extends Controller
